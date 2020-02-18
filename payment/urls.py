dc98fd6717e2be38b5a2485190c667083dfbb0fa
Mohansunki <58895671+mohansunka@users.noreply.github.com>
2020-02-18 18:10:21 +0530

    Delete urls.py

--- payment/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-        path('charge/', views.charge, name='charge'),
-# new
-        path('', views.HomePageView.as_view(), name='home'),
-]
